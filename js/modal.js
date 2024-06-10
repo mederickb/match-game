dcb0b19e988e0b5464c09a1344d0d66c4fb603c3
mederickb <blouinmederick@gmail.com>
2024-06-10 13:52:23 -0400

    changed basic button styles

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const helpButton = document.getElementById('helpButton');
-    const helpModal = document.getElementById('helpModal');
-    const closeModal = document.getElementsByClassName('close')[0];
+	//const helpButton = document.getElementById('helpButton');
+	const helpModal = document.getElementById('helpModal');
+	const closeModal = document.getElementsByClassName('close')[0];
 
-    helpButton.onclick = function() {
-        helpModal.style.display = 'block';
-    }
+	helpButton.onclick = function() {
+		helpModal.style.display = 'block';
+	}
 
-    closeModal.onclick = function() {
-        helpModal.style.display = 'none';
-    }
+	closeModal.onclick = function() {
+		helpModal.style.display = 'none';
+	}
 
-    window.onclick = function(event) {
-        if (event.target == helpModal) {
-            helpModal.style.display = 'none';
-        }
-    }
+	window.onclick = function(event) {
+		if (event.target == helpModal) {
+			helpModal.style.display = 'none';
+		}
+	}
 });
