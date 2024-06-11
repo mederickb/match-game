0e16e7cb1f4e622008ee539a86071f9e8e9b9aaa
mederickb <blouinmederick@gmail.com>
2024-06-11 04:31:19 -0400

    a whole bunch of changes to resolve merge conflict

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-	//const helpButton = document.getElementById('helpButton');
-	const helpModal = document.getElementById('helpModal');
-	const closeModal = document.getElementsByClassName('close')[0];
+	const modalButton = document.getElementById('helpButton');
+	const modalHelp = document.getElementById('modalHelp');
+	const modalClose = document.getElementsByClassName('modalClose')[0];
 
-	helpButton.onclick = function() {
-		helpModal.style.display = 'block';
+	modalButton.onclick = function() {
+		modalHelp.style.display = 'block';
 	}
 
-	closeModal.onclick = function() {
-		helpModal.style.display = 'none';
+	modalClose.onclick = function() {
+		modalHelp.style.display = 'none';
 	}
 
 	window.onclick = function(event) {
-		if (event.target == helpModal) {
-			helpModal.style.display = 'none';
+		if (event.target == modalHelp) {
+			modalHelp.style.display = 'none';
 		}
 	}
 });
